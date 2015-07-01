3781614b7d2329a60f8e3752271511599818c19d
Adam Scott <ascott1@gmail.com>
2015-07-01 06:55:10 -0400

    add livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 
 gulp.task('default', function() {
-  console.log('Your first task has run!')
+  browserSync.init({
+    server: './'
+  });
+  gulp.watch('src/scripts/**/*.js', browserSync.reload);
+  gulp.watch('src/styles/**/*.css', browserSync.reload);
+  gulp.watch('src/img/**/*', browserSync.reload);
+  gulp.watch('*.html', browserSync.reload);
 });
