e1efe74a53753d1061723ffcb1adfffd46f34eda
Adam Scott <ascott1@gmail.com>
2015-07-01 07:33:50 -0400

    js task

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,32 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var minifyCss = require('gulp-minify-css');
 var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
 
 gulp.task('styles', function() {
   gulp.src(['src/styles/**/*.css'])
-  .pipe(sourcemaps.init())
-  .pipe(gulp.dest('dist/styles'))
-  .pipe(minifyCss())
-  .pipe(sourcemaps.write())
-  .pipe(gulp.dest('dist/styles'))
-  .pipe(browserSync.stream());
+    .pipe(sourcemaps.init())
+    .pipe(gulp.dest('dist/styles'))
+    .pipe(minifyCss())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/styles'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('scripts', function() {
+  gulp.src(['src/scripts/main.js'])
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('default', function() {
   browserSync.init({
     server: './'
   });
-  gulp.watch('src/scripts/**/*.js', browserSync.reload);
+  gulp.watch('src/scripts/**/*.js', ['scripts']);
   gulp.watch('src/styles/**/*.css', ['styles']);
   gulp.watch('src/img/**/*', browserSync.reload);
   gulp.watch('*.html', browserSync.reload);
