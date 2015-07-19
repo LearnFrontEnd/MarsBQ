50b6dbcf20d484df7c899e4d204d6556a6daa82d
Adam Scott <ascott1@gmail.com>
2015-07-19 09:42:53 -0400

    set up model view helper structure

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
-console.log('HELLO WORLD');
+var model = {
+  init: function(){
+    console.log('Model initialized');
+  }
+};
+
+var helper = {
+
+};
+
+var view = {
+  init: function() {
+    console.log('View initialized');
+  }
+};
+
+var app = {
+  init: function() {
+    model.init();
+    view.init();
+  }
+};
+
+app.init();
