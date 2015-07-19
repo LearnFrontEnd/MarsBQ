76fda22d386c940ce8e9024caf9ea1ea6a9c9003
Adam Scott <ascott1@gmail.com>
2015-07-19 16:50:47 -0700

    js for menu clicks

--- src/scripts/modules/helper.js
+++ src/scripts/modules/helper.js
@@ -5,6 +5,23 @@ var helper = module.exports = {
 
   dateDiff: function() {
     return countdown(model.currentDate, model.eventDate);
+  },
+
+  setCurrent: function(id) {
+    for (var i = 0; i < model.menu.length; i++) {
+      if (model.menu[i].id === id) {
+        model.menuItem = model.menu[i];
+      }
+    }
+  },
+
+  /* increment the menu item */
+  increment: function() {
+    model.menuItem.count++;
+  },
+
+  getCurrent: function() {
+    return model.menuItem;
   }
 
 };
--- src/scripts/modules/view.js
+++ src/scripts/modules/view.js
@@ -4,6 +4,7 @@ var view = module.exports = {
 
   init: function() {
     view.showCountdown();
+    view.registerClick();
   },
 
   showCountdown: function() {
@@ -12,6 +13,29 @@ var view = module.exports = {
     document.querySelector('#years').innerHTML = diff.years;
     document.querySelector('#days').innerHTML = diff.days;
     document.querySelector('#hours').innerHTML = diff.hours;
-  }
+  },
+
+  registerClick: function() {
+    var id;
+    var listItem = document.querySelectorAll('#menu li');
+
+    for(var i = 0; i < listItem.length; i++) {
+      listItem[i].addEventListener('click', function(){
+        id = this.getAttribute('data-id');
+        // add + to make sure the value is an integer
+        helper.setCurrent(+id);
+        helper.increment();
+        view.updateCount();
+      });
+    }
+  },
+
+    updateCount: function() {
+      var item = helper.getCurrent();
+      var li = document.querySelector('[data-id="' + item.id + '"]');//$('li[data-id=' + item.id + ']');
+      var count = li.querySelector('.menu-count');
+
+      count.innerHTML = item.count;
+    }
 
 };
