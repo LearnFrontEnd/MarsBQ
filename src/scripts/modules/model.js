79cad368e05db719ae9fe3cbae6abee0ce35196e
Adam Scott <ascott1@gmail.com>
2015-07-19 16:35:08 -0700

    set up menu models

--- src/scripts/modules/model.js
+++ src/scripts/modules/model.js
@@ -1,9 +1,29 @@
+var request = require('superagent');
+
 var model = module.exports = {
   init: function(){
-    console.log('Model initialized');
+    model.getData();
   },
 
   /* current and future dates */
   currentDate: new Date(),
-  eventDate: new Date(2056, 10, 05)
+  eventDate: new Date(2056, 10, 05),
+
+  /* menu is empty until data is fetched */
+  menu: {},
+
+  /* model for the current menu item */
+  menuItem: {
+    title: null,
+    count: null,
+    id: null
+  },
+
+  /* get the data */
+  getData: function() {
+    request.get('menu.json', function(err, res) {
+      model.menu = JSON.parse(res.text).menuItems;
+    })
+  }
+
 };
